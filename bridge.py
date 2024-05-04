0726a4f7f1de116a6e96a1a06200767435079327
Nick McComb <nick@soeffects.com>
2024-05-04 15:32:56 -0700

    Add ChatGPT baseline
    https://chat.openai.com/share/4717609c-7bc9-4daa-a87f-645cda333697

---
+++ bridge.py
@@ -0,0 +1,50 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+import argparse
+import sys
+
+def authenticate_gspread(json_creds):
+    scope = ['https://www.googleapis.com/auth/spreadsheets']
+    creds = ServiceAccountCredentials.from_json_keyfile_name(json_creds, scope)
+    client = gspread.authorize(creds)
+    return client
+
+def upload_to_sheet(json_creds, file_path, url, sheet_name, start_cell):
+    client = authenticate_gspread(json_creds)
+    sheet = client.open_by_url(url)
+    worksheet = sheet.worksheet(sheet_name)
+    data = pd.read_csv(file_path)
+    cell_list = worksheet.range(start_cell + ':' + gspread.utils.rowcol_to_a1(data.shape[0] + worksheet.range(start_cell)[0].row - 1,
+                                                                            data.shape[1] + worksheet.range(start_cell)[0].col - 1))
+    flat_list = data.values.flatten()
+    for cell, value in zip(cell_list, flat_list):
+        cell.value = value
+    worksheet.update_cells(cell_list)
+
+def download_from_sheet(json_creds, url, sheet_name, start_cell, output_file):
+    client = authenticate_gspread(json_creds)
+    sheet = client.open_by_url(url)
+    worksheet = sheet.worksheet(sheet_name)
+    data = worksheet.get(start_cell + ':' + gspread.utils.rowcol_to_a1(worksheet.row_count,
+                                                                      worksheet.col_count))
+    df = pd.DataFrame(data)
+    df.to_csv(output_file, index=False)
+
+def main():
+    parser = argparse.ArgumentParser(description='Upload or download a CSV to/from Google Sheets.')
+    parser.add_argument('action', choices=['upload', 'download'], help='Action to perform: upload or download')
+    parser.add_argument('json_creds', help='Path to JSON credentials file')
+    parser.add_argument('file_path', help='Path to the CSV file for upload or destination for download')
+    parser.add_argument('url', help='URL of the Google Sheet')
+    parser.add_argument('sheet_name', help='Name of the worksheet in the Google Sheet')
+    parser.add_argument('start_cell', help='Start cell for upload or download')
+    args = parser.parse_args()
+
+    if args.action == 'upload':
+        upload_to_sheet(args.json_creds, args.file_path, args.url, args.sheet_name, args.start_cell)
+    elif args.action == 'download':
+        download_from_sheet(args.json_creds, args.url, args.sheet_name, args.start_cell, args.file_path)
+
+if __name__ == "__main__":
+    main()
